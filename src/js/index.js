afdcc095fbc46e4fb69bb3f0d2a7eb155a499d7c
ccahuber13 <c.c.a.huber@gmail.com>
2019-08-06 15:14:55 -0700

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+// Global app controller
+import './test'
+import '../styles/scss/appStyles.scss'
+import '../styles/css/main.css'
+
+console.log(`Imported ${x} from another module`);
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: ['babel-polyfill','./src/js/index.js'],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: './css/main.css',
+            chunkFilename: '[id].css'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
+            }
+           
+        ]
+    },
+
+};
+
